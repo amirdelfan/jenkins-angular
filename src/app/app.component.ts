c77f0db5db5df869a7b11db571cd08569978d99f
Amir Delfan <amirdelfan@live.nl>
2022-10-19 22:54:44 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent2 {
+export class AppComponent {
   title = 'sample';
 }
